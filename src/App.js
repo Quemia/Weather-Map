530169b1748d5f11022e9f8d721acf6f24c78adb
Quemia <que.carol@hotmail.com>
2022-01-26 17:59:57 -0300

    Added react hooks and api

--- src/App.js
+++ src/App.js
@@ -1,33 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CardTemp from "./components/cards/card";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import axios from "axios";
 
 function App() {
-  return (
-    <Container maxWidth="md">
-      <Box sx={{ flexGrow: 1 }}>
-        <Toolbar>
-          <Typography
-            variant="h4"
-            component="div"
-            sx={{
-              my: 2,
-              flexGrow: 1,
-              textAlign: "center",
-              fontFamily: "Outfit",
-            }}
-            color="#042d54"
-          >
-            Clima nas suas coordenadas:
-          </Typography>
-        </Toolbar>
-      </Box>
-      <CardTemp />
-    </Container>
-  );
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get(
+      "http://api.openweathermap.org/data/2.5/weather",
+      {
+        params: {
+          lat: lat,
+          lon: long,
+          appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+          lang: "pt",
+          units: "metric",
+        },
+      }
+    );
+    setWeather(res.data);
+    console.log(res);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    });
+  }, []);
+
+  if (location === false) {
+    return (
+      <Typography
+        variant="h4"
+        component="div"
+        sx={{
+          my: 2,
+          flexGrow: 1,
+          textAlign: "center",
+          fontFamily: "Outfit",
+        }}
+        color="#042d54"
+      >
+        Você precisa habilitar a localização no browser
+      </Typography>
+    );
+  } else if (weather === false) {
+    return (
+      <Typography
+        variant="h4"
+        component="div"
+        sx={{
+          my: 2,
+          flexGrow: 1,
+          textAlign: "center",
+          fontFamily: "Outfit",
+        }}
+        color="#042d54"
+      >
+        Carregando o clima...
+      </Typography>
+    );
+  } else {
+    return (
+      <Container maxWidth="md">
+        <Box sx={{ flexGrow: 1 }}>
+          <Toolbar>
+            <Typography
+              variant="h4"
+              component="div"
+              sx={{
+                my: 2,
+                flexGrow: 1,
+                textAlign: "center",
+                fontFamily: "Outfit",
+              }}
+              color="#042d54"
+            >
+              Clima nas suas coordenadas:
+            </Typography>
+          </Toolbar>
+        </Box>
+        <CardTemp weather={weather} />
+      </Container>
+    );
+  }
 }
 
 export default App;
--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -2,7 +2,7 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
-const CardTemp = () => {
+const CardTemp = ({ weather }) => {
   return (
     <Box
       sx={{
@@ -16,29 +16,29 @@ const CardTemp = () => {
       }}
     >
       <Typography sx={{ fontSize: 20, my: 1 }} color="#fff" gutterBottom>
-        Timon, Maranhão
+        {weather["name"]}, {weather["sys"]["country"]}
       </Typography>
       <Typography variant="h2" color="#fff" component="div">
-        34ºC
+        {weather["main"]["temp"]}ºC
       </Typography>
       <Typography sx={{ mt: 1, mb: 2.5 }} color="#ffffffc2">
-        Parcialmente ensolarado
+        {weather["weather"][0]["description"]}
       </Typography>
       <Typography variant="body2">
         <Typography sx={{ fontSize: 17 }} color="#ffffffc2" gutterBottom>
-          Temperatura atual: x ºC
+          Sensação Térmica: {weather["main"]["feels_like"]} ºC
         </Typography>
         <Typography sx={{ fontSize: 17 }} color="#ffffffc2" gutterBottom>
-          Temperatura máxima: x ºC
+          Temperatura máxima: {weather["main"]["temp_max"]} ºC
         </Typography>
         <Typography sx={{ fontSize: 17 }} color="#ffffffc2" gutterBottom>
-          Temperatura mínima: x ºC
+          Temperatura mínima: {weather["main"]["temp_min"]} ºC
         </Typography>
         <Typography sx={{ fontSize: 17 }} color="#ffffffc2" gutterBottom>
-          Pressão: x hps
+          Pressão: {weather["main"]["pressure"]} hpa
         </Typography>
         <Typography sx={{ fontSize: 17 }} color="#ffffffc2" gutterBottom>
-          Umidade: x%
+          Umidade: {weather["main"]["humidity"]} %
         </Typography>
       </Typography>
     </Box>
